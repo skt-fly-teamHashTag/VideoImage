966654b5940393aaf86508b6f13f6548b94b53f7
rkskek1226 <hkcho01@naver.com>
2023-02-18 22:34:07 +0900

    make_thumbnail.py

--- Segmentation/make_thumbnail.py
+++ Segmentation/make_thumbnail.py
@@ -4,17 +4,20 @@ from make_mask_img import make_mask_img
 from make_thumbnail_fg import make_thumbnail_fg
 from make_thumbnail_bg1 import make_thumbnail_bg1
 from make_thumbnail_bg2 import make_thumbnail_bg2
+from plus_sticker import plus_sticker
 
 
 a = Thumbnail(q1[:5].tolist(), step=5)
 a.forward()
 vlog_message = "QWER VLOG"
+category = "family"
 
 img_case, mask_img = make_mask_img(outputs=a.outputs, input_data=a.input_data)
 
 if img_case != 4:
     dst = make_thumbnail_fg(img_case, mask_img)
     dst = make_thumbnail_bg1(dst1=dst, bg_image=a.background_img1, bg_c="sky", text_f="base", text_c="white",text=vlog_message, font_scale=2, font_thickness=2)
+    dst = plus_sticker(category=category, dst=dst)
 else:
     background_img_list = []
 
@@ -26,3 +29,6 @@ else:
     with torch.no_grad():
         background_output = a.model(torch_bg_list)
     dst = make_thumbnail_bg2(background_img=a.background_img, background_output=background_output, text_f="base", text_c="white", text=vlog_message, font_scale=2, font_thickness=2)
+
+
+cv2.imwrite("dst.jpg", dst)
