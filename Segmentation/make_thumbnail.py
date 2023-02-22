f5ab01e8169bf94af277e4947e33474383b869fc
rkskek1226 <hkcho01@naver.com>
2023-02-23 01:29:42 +0900

    make_thumbnail.py

--- Segmentation/make_thumbnail.py
+++ Segmentation/make_thumbnail.py
@@ -12,12 +12,16 @@ from make_thumbnail_lovely import make_thumbnail_lovely
 from make_thumbnail_modern import make_thumbnail_modern
 
 
-def make_thumbnail(thumb_numpy, nickname, category):
+def make_thumbnail(thumb_numpy, nickname, category_list):
     a = Thumbnail(thumb_numpy.tolist(), step=5)
     vlog_message = nickname
-    category = category
-    dst_list = []
+    category = -1
     # category_dic = {0: "가족", 1: "스터디", 2: "뷰티", 3: "반려동물", 4: "운동/스포츠", 5: "음식", 6: "여행", 7: "연애/결혼", 8: "문화생활", 9: "직장인"}
+
+    if random.choice([0, 1]) == 0:
+        category = category_list[0]
+    else:
+        category = category_list[1]
     
     a.forward()
     img_case, outputs, input_data = mask_preprocess(outputs=a.outputs, input_data=a.input_data)
@@ -33,27 +37,21 @@ def make_thumbnail(thumb_numpy, nickname, category):
         with torch.no_grad():
             background_output = a.model(torch_bg_list)
         dst = make_thumbnail_bg2(background_img=a.background_img, background_output=background_output, text_f="base", text_c="white", text=vlog_message, font_scale=2, font_thickness=2)
-        dst_list.append(dst)
 
     else:
-        for c in category:
-            if c in [1, 4, 9]:
-                dst = make_thumbnail_modern(input_data=input_data, outputs=outputs)
-                dst_list.append(dst)
+        if category in [1, 4, 9]:
+            dst = make_thumbnail_modern(input_data=input_data, outputs=outputs)
+        else:
+            mask_img = make_mask_img(outputs=outputs, input_data_img=input_data)
+            dst = make_thumbnail_fg(img_case, mask_img)
+            dst = make_thumbnail_bg1(dst1=dst, bg_image=a.background_img1, bg_c="sky", text_f="base", text_c="white",text=vlog_message, font_scale=2, font_thickness=2)
+            dst = cv2.resize(dst, (780, 430))
+            if category in [8, 0, 5, 3, 6]:
+                dst = make_thumbnail_daily(img=dst)
             else:
-                mask_img = make_mask_img(outputs=outputs, input_data_img=input_data)
-                dst = make_thumbnail_fg(img_case, mask_img)
-                dst = make_thumbnail_bg1(dst1=dst, bg_image=a.background_img1, bg_c="sky", text_f="base", text_c="white",text=vlog_message, font_scale=2, font_thickness=2)
-                dst = cv2.resize(dst, (780, 430))
-                if c in [8, 0, 5, 3, 6]:
-                    dst = make_thumbnail_daily(img=dst)
-                else:
-                    dst = make_thumbnail_lovely(img=dst)
-                dst_list.append(dst)
+                dst = make_thumbnail_lovely(img=dst)
 
-    if random.choice([0, 1]) == 0:
-        return dst_list[0]
-    else:
-        return dst_list[1]
+    return dst
+
 
     
